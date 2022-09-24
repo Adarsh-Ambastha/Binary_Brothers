05b03f6ff3fcc95971a8b668a9b5365e2a38743e
Adarsh Ambastha <72976932+Adarsh-Ambastha@users.noreply.github.com>
2022-09-24 20:15:05 +0530

    Initial Git Upload

---
+++ rationcard/clientside/loginpage/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/grids-responsive-min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="pure-g">
+
+    </div>
+    <div class="pure-g">
+        <div class="pure-u-1-5"></div>
+        <div id="loginbox" class="pure-u-3-5">
+            <form class="pure-form pure-form-stacked">
+                <fieldset>
+                    <input style="margin-left: auto; margin-right: auto" type="text" placeholder="Enter your Aadhar" />
+                    <a href="../homepage" style="margin-top: 0.5em;" type="submit" class="pure-button pure-button-primary">Log in using Aadhar</a>
+                </fieldset>
+                OR
+                <fieldset>
+                    <a href="../homepage" style="margin-top: 0.5em;" type="submit" class="pure-button pure-button-primary">Log in using Digilocker</a>
+                </fieldset>
+            </form>
+        </div>
+        <div class="pure-u-1-5"></div>
+    </div>
+</body>
+</html>
---
+++ rationcard/serverside/components/header.php
@@ -0,0 +1,33 @@
+<div id="header" class="pure-g">
+    <div class="pure-u-lg-1-24 pure-u-sm-1-2">
+        <img id="logo" src="../components/assets/logo.png" alt="Application Logo">
+    </div>
+    <div id="appname" class="pure-u-lg-3-24 pure-u-sm-1-2">
+        <h3>Project Name</h3>
+    </div>
+    <div class="vertical-black"></div>
+    <div class="pure-u-lg-6-24 pure-u-sm-1-3">
+        <h3 style="padding-left: 1em">DASHBOARD</h3>
+    </div>
+    <div class="pure-u-lg-6-24 pure-u-sm-2-3">
+        <img id="search" src="../components/assets/search.png" alt="Search Bar">
+    </div>
+    <div class="vertical-black"></div>
+    <div style="padding-left: 0.5em;" class="pure-u-lg-1-24 pure-u-sm-1-3 alerts">
+        <img src="../components/assets/message.png" alt="Message Icon">
+    </div>
+    <div class="pure-u-lg-1-24 pure-u-sm-1-3 alerts">
+        <img src="../components/assets/notification.png" alt="Notification Icon">
+    </div>
+    <div class="pure-u-lg-1-24 pure-u-sm-1-3 alerts">
+        <img style="width:55%" src="../components/assets/calendar.png" alt="Calendar Icon">
+    </div>
+    <div style="margin-left: -0.5em" class="vertical-black"></div>
+    <div style="padding-left: 1em; padding-top: 0.5em;" class="pure-u-lg-1-24 pure-u-sm-1-3">
+        <img  id="usericon" src="../components/assets/usericon.png" alt="User Icon">
+    </div>
+    <div style="margin-top: -0.25em" class="pure-u-lg-3-24 pure-u-sm-12-3">
+        <p style="font-weight: bold">Khilav Jadav</p>
+        <p style="margin-top: -1em">Inventory Manager</p>
+    </div>
+</div>
